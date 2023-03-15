5950b5f7512182b233b16f04e87e8cad8cbfcda1
Hikmot-O <hikmotokunola@gmail.com>
2023-03-15 13:06:02 +0100

    setup

---
+++ vetting-dashboard/src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const ErrorPage = React.lazy(() => import('./pages/Error'));
+const Login = React.lazy(() => import('./Auth/Login'));
+
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: (
+      <React.Suspense fallback={<>...</>}>
+        <Login />
+      </React.Suspense>
+    ),
+    errorElement: (
+      <React.Suspense fallback={<>...</>}>
+        <ErrorPage />
+      </React.Suspense>
+    ),
+  },
+  // {
+  //   path: 'dashboard',
+  //   element: (
+  //     <React.Suspense fallback={<>...</>}>
+  //       {/* <Dashboard /> */}
+  //     </React.Suspense>
+  //   ),
+  //   errorElement: (
+  //     <React.Suspense fallback={<>...</>}>
+  //       <ErrorPage />
+  //     </React.Suspense>
+  //   ),
+  //   loader: checkAuthLoader,
+  // },
+])
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
